77da710f8ac795fd475e5fd35fb4d7ff3870374e
rajaditya007 <91844072+rajaditya007@users.noreply.github.com>
2024-08-14 06:15:20 +0530

    Update LoginActivity.java

--- CaretakersDesk/app/src/main/java/com/aditya1/caretakersdesk/LoginActivity.java
+++ CaretakersDesk/app/src/main/java/com/aditya1/caretakersdesk/LoginActivity.java
@@ -49,20 +49,6 @@ public class LoginActivity extends AppCompatActivity {
         user = FirebaseAuth.getInstance().getCurrentUser();
         loginPageQuestion= findViewById(R.id.loginPageQuestion);
         loginPageQuestion.setOnClickListener(new View.OnClickListener()
-
-
-         Retrofit retrofit = new Retrofit.Builder()
-    .baseUrl("http://<VM-IP>:8080/")  // Replace with your VM's IP address
-    .addConverterFactory(GsonConverterFactory.create())
-    .build();
-
-// Define your API endpoints interface
-public interface ApiService {
-    @GET("api/data")
-    Call<List<Data>> getData();
-}
-
-ApiService apiService = retrofit.create(ApiService.class);
                                     
 
 
